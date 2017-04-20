5830ca9e10001396bc1734f34df8c8967816af4e
Ketho <sghs17@outlook.com>
2017-04-20 10:29:20 +0200

    Added Nighthold -> Suramar

--- FixOrderHallMap.lua
+++ FixOrderHallMap.lua
@@ -1,19 +1,21 @@
 -- License: Public Domain
 
 local t = {
-	[23] = function() return select(4, GetMapInfo()) end, -- Paladin, Sanctum of Light; Eastern Plaguelands
-	[1040] = function() return true end, -- Priest, Netherlight Temple; Azeroth
-	[1044] = function() return true end, -- Monk, Temple of Five Dawns; none
-	[1048] = function() return true end, -- Druid, Emerald Dreamway; none
-	[1052] = function() return GetCurrentMapDungeonLevel() > 1 end, -- Demon Hunter, Fel Hammer; Mardum
+	[23] = function() return select(4, GetMapInfo()) and 1007 end, -- Paladin, Sanctum of Light; Eastern Plaguelands
+	[1040] = function() return 1007 end, -- Priest, Netherlight Temple; Azeroth
+	[1044] = function() return 1007 end, -- Monk, Temple of Five Dawns; none
+	[1048] = function() return 1007 end, -- Druid, Emerald Dreamway; none
+	[1052] = function() return GetCurrentMapDungeonLevel() > 1 and 1007 end, -- Demon Hunter, Fel Hammer; Mardum
+	[1088] = function() return GetCurrentMapDungeonLevel() == 3 and 1033 end, -- Nighthold -> Suramar
 }
 
 local OnClick = WorldMapZoomOutButton_OnClick
 
 function WorldMapZoomOutButton_OnClick()
 	local id = t[GetCurrentMapAreaID()]
-	if id and id() then
-		SetMapByID(1007) -- Broken Isles
+	local out = id and id()
+	if out then
+		SetMapByID(out)
 	else
 		OnClick()
 	end
